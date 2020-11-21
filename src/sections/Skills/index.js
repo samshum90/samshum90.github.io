ffbfd56f5b780ea537c10ebbd9ab37e2c7af0a0e
Sam <samshum90@gmail.com>
2020-11-21 22:53:26 +0000

    started skills

--- src/sections/Skills/index.js
+++ src/sections/Skills/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 
+import skillsList from "./skillsList";
+import "./skills.scss";
+
 const Skills = () => {
-    return (
+  return (
     <>
-            My Skills!
-        </>
-    )
-}
+      {skillsList.map(({ skills, title }) => (
+        <article className="skills-wrapper" key={title}>
+          <h4 className="skills__title">{title}</h4>
+          <ul className="skills__list">
+            {skills.map(({ name, icon }) => (
+              <li key={name} className="skills__list-item">
+                <img className="skills__icon" src={icon} alt={name} />
+                <span className="skills__name">{name}</span>
+              </li>
+            ))}
+          </ul>
+        </article>
+      ))}
+    </>
+  );
+};
 
 export default Skills;
---
+++ src/sections/Skills/skillsList.js
@@ -0,0 +1,71 @@
+import {
+  html,
+  sass,
+  javascript,
+  vue,
+  react,
+  angular,
+  typescript,
+  ruby,
+  java,
+  node,
+  mongodb,
+  firebase,
+  postgresql,
+  python,
+  ionic,
+  cSharp,
+  net,
+  xd,
+  inDesign,
+  illustrator,
+  lightroom,
+  photoshop,
+} from "../../assets/badges/SVG";
+
+const skillsList = [
+  {
+    title: "My tech stack",
+    skills: [
+      { name: "HTML 5", icon: html },
+      { name: "CSS 3/SCSS", icon: sass },
+      { name: "Javascript", icon: javascript },
+      { name: "Vue", icon: vue },
+      { name: "React", icon: react },
+      { name: "Angular", icon: angular },
+      { name: "Typescript", icon: typescript },
+      { name: "Ruby", icon: ruby },
+      { name: "Java", icon: java },
+      { name: "Node Js", icon: node },
+      { name: "Mongo DB", icon: mongodb },
+      { name: "Firebase", icon: firebase },
+      { name: "Postgres SQL", icon: postgresql },
+    ],
+  },
+  {
+    title: "Tech I've worked with",
+    skills: [
+      { name: "Python", icon: python },
+      { name: "Ionic", icon: ionic },
+    ],
+  },
+  {
+    title: "Currently learning",
+    skills: [
+      { name: "C#", icon: cSharp },
+      { name: ".NET", icon: net },
+    ],
+  },
+  {
+    title: "Design tools",
+    skills: [
+      { name: "XD", icon: xd },
+      { name: "InDesign", icon: inDesign },
+      { name: "Illustrator", icon: illustrator },
+      { name: "Lightroom", icon: lightroom },
+      { name: "Photoshop", icon: photoshop },
+    ],
+  },
+];
+
+export default skillsList;
