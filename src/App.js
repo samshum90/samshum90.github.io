173fb9da857f812a9a2b1c1e55c05b11ffa40d06
Sam <samshum90@gmail.com>
2020-11-22 00:15:32 +0000

    added skills section

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import { Home } from "./containers";
 import { About, Header, Projects, Skills } from "./sections";
 
 const sections = [
-  { name: 'home', Component: Header, title: 'Home', navName: 'Home' },
-  { name: 'skills', Component: Skills, title: 'Tech I know', navName: 'Tech' },
-  { name: 'projects', Component: Projects, title: 'Projects', navName: 'Projects' },
-  { name: 'about', Component: About, title: 'About me', navName: 'About' },
+  { name: "home", Component: Header, title: "Home", navName: "Home" },
+  { name: "skills", Component: Skills, title: "Tech I know", navName: "Tech" },
+  {
+    name: "projects",
+    Component: Projects,
+    title: "Projects",
+    navName: "Projects",
+  },
+  { name: "about", Component: About, title: "About me", navName: "About" },
 ];
 
 function App() {
   return (
     <>
-      <Nav sections={sections}/>
-      <Home sections={sections}/>
+      <Nav sections={sections} />
+      <Home sections={sections} />
       <Footer />
     </>
   );
