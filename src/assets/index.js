a632d63f007cd3e1ad94cbed8ceda2e321d63efe
Sam <samshum90@gmail.com>
2020-11-23 14:07:01 +0000

    added in the footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,3 +2,6 @@ export { default as banner } from "./banner2.jpg";
 export { default as github } from "./github.svg";
 export { default as globe } from "./globe.svg";
 export { default as devpost } from "./devpost.svg";
+export { default as envelope } from "./envelope.svg";
+export { default as linkedin } from "./linkedin.svg";
+export { default as pdf } from "./pdf.svg";
